46e6795e64c70aa2fd4249a3a54eb86c41853f8d
wokersilva <marcosilva@ciencias.unam.mx>
2024-04-09 18:31:27 -0600

    Añadiendo función: editar notas

--- proyectoTablero/apps/tablero/urls.py
+++ proyectoTablero/apps/tablero/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import crearNota, Notas
+from .views import crearNota, Notas, editarNota
 
 urlpatterns = [
     path('crear_nota/', crearNota, name='crear_nota'),
-    path('notas/', Notas, name='notas' )
+    path('notas/', Notas, name='notas' ),
+    path('editar_nota/<int:id>', editarNota, name= 'editar_nota')
 ]
--- proyectoTablero/apps/tablero/views.py
+++ proyectoTablero/apps/tablero/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render,redirect
-from .models import Nota
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import NotaForm
-# Create your views here.
+from .models import Nota
+
+# Esta función nos carga el arhivo raiz 
+#  es la vista principal de la aplicación 
 def Home(request):
     return render(request, 'index.html')
 
+# Esta función carga el archivo de notas 
+#  y tambien hace la lista de las notas en la BD
 def Notas(request):
     notas = Nota.objects.all()
     return render(request, 'tablero/notas.html',{'notas':notas})
 
+# Esta función carga la pagina para crear las notas
 def crearNota(request):
     if request.method == "POST":
         nota_form = NotaForm(request.POST)
@@ -19,3 +25,19 @@ def crearNota(request):
         nota_form = NotaForm()
     return render(request, 'tablero/crear_nota.html',{'nota_form':nota_form})
 
+def editarNota(request, id):
+    nota_form = None
+    error = None
+    # Manejamos el error de no encontrar el id 
+    try:
+        nota = Nota.objects.get(id = id)        
+        if request.method == 'GET':
+            nota_form = NotaForm(instance = nota)
+        else:
+            nota_form = NotaForm(request.POST, instance = nota)
+            if nota_form.is_valid():
+                nota_form.save()
+            return redirect('notas')    
+    except ObjectDoesNotExist as e:
+        error = e
+    return render(request,'tablero/crear_nota.html',{'nota_form':nota_form,'error':error})
