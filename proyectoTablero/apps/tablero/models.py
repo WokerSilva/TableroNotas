cb263cf86b8199c486fe939ed8c4299a2d65b898
wokersilva <marcosilva@ciencias.unam.mx>
2024-04-17 00:12:18 -0600

    Creando migracion de imagenes en BD

--- proyectoTablero/apps/tablero/models.py
+++ proyectoTablero/apps/tablero/models.py
@@ -7,6 +7,13 @@ class Nota (models.Model):
     id = models.AutoField(primary_key=True) # ESTA ES NUESTRA LLAVE PRIMARIA 
     titulo = models.CharField(max_length=50, blank=False, null=False) # Nuestro titulo no se puede quedar en blanco
     contenido = models.TextField(blank=False, null=False) # El contenido no puede ser vacio
+    imagen = models.ImageField(upload_to='notas_imagenes/', null=True, blank=True) 
+    COLORES = [
+        ('#ff0000', 'Rojo'),  
+        ('#00ff00', 'Verde'),  
+        ('#0000ff', 'Azul'),  
+    ]
+    color = models.CharField(max_length=20, choices=COLORES, default='#ffffff')  
 
     def __str__(self):
         return self.titulo 
