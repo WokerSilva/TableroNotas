1c7cd360b47fc2e93f95fc503d9d1d597fd5fe8f
wokersilva <marcosilva@ciencias.unam.mx>
2024-04-21 20:27:01 -0600

    Advertencia contraseñas

--- proyectoTablero/apps/usuarios/forms.py
+++ proyectoTablero/apps/usuarios/forms.py
@@ -7,7 +7,18 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('username', 'password1', 'password2', 'fecha_nacimiento')
+        #fields = ('username', 'password1', 'password2', 'fecha_nacimiento')
+        fields = ['username', 'fecha_nacimiento', 'email', 'password1', 'password2']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.")
+
+        return cleaned_data
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
